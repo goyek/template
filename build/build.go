0bc7b6cbc6ed702f4bcda1b41cd47f2f5f4a5a5b
Robert Pająk <pellared@hotmail.com>
2022-10-28 19:47:33 +0200

    Remove whitespaces in build task

--- build/build.go
+++ build/build.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os/exec"
+	"strings"
 
 	"github.com/goyek/goyek/v2"
 )
@@ -10,10 +11,11 @@ var build = goyek.Define(goyek.Task{
 	Name:  "build",
 	Usage: "go build",
 	Action: func(tf *goyek.TF) {
-		ext, err := exec.CommandContext(tf.Context(), "go", "env", "GOEXE").CombinedOutput()
+		extBytes, err := exec.CommandContext(tf.Context(), "go", "env", "GOEXE").CombinedOutput()
 		if err != nil {
 			tf.Fatal(err)
 		}
-		Exec(tf, dirRoot, `go build -ldflags="-s -w" -o dist/app`+string(ext))
+		ext := strings.TrimSpace(string(extBytes))
+		Exec(tf, `go build -ldflags="-s -w" -o dist/app`+ext)
 	},
 })
